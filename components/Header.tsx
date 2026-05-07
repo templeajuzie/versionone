4499981708e2eb990c5376bd15e0ee87af2beb26
Temple <templeajuzie@gmail.com>
2026-05-07 17:28:35 +0100

    feat: update

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,7 +41,7 @@ export function Header() {
   ];
 
   return (
-    <header className="fixed top-0 right-0 left-0 z-50 w-full transition-all duration-300">
+    <header className="fixed top-0 right-0 left-0 z-50 w-full bg-white transition-all duration-300">
       {/* Top Utility Bar */}
       <div
         className={cn(
